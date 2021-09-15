3dce2cb6c7ed31078350b9591b1c32c6c991b971
Meowyam <maryam@chilon.net>
2021-09-15 08:51:25 +0800

    hide and show

--- src/views/Prototype.vue
+++ src/views/Prototype.vue
@@ -1,22 +1,34 @@
 <template>
-  <section class="section is-medium">
+  <section class="section">
     <div class="container">
-    <h1 class="title">
-      Did you...
-    </h1>
-    <div v-for="action in actions" v-bind:key="action.index">
-      <div class="columns is-centered">
-      <div class="column is-half subtitle">{{action}}</div>
-      <div class="column field has-addons"
-       v-for="(val, key) in checkOptions" :key="key">
-        <label class="radio">
-          <input type="radio" name="check" :value="val" :checked="val == value"
-           @change=goNext(action,val) />
-          {{val}}
-        </label>
+      <div class="title">
+        Did you...
       </div>
+      <div v-for="action in actions" v-bind:key="action.index">
+        <div class="columns is-centered" :id="action">
+          <div class="column is-half subtitle">{{action}}</div>
+          <div class="column field has-addons"
+          v-for="(val, key) in checkOptions" :key="key">
+            <label class="radio">
+              <input type="radio" :name="action" :value="val"
+              @change=goNext(action,val) />
+              {{val}}
+            </label>
+          </div>
+        </div>
       </div>
     </div>
+  </section>
+  <section class="section" v-if="consSection">
+    <div class="container">
+      <div class="title">
+        Then you...
+      </div>
+      <div v-for="consequence in consequences" v-bind:key="consequence.index">
+        <div class="subtitle">
+          {{consequence}}
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -25,19 +37,28 @@
 export default {
   data() {
     return {
-      actions: ['walk', 'run'],
+      actions: ['walk', 'run', 'jog'],
       consequences: ['sing'],
       checkOptions: ['yes', 'no', 'don\'t know'],
       eachQuestion: [],
       i: Number,
       checked: [],
       act: [],
+      consSection: false,
     };
   },
   methods: {
     goNext(action, selection) {
       console.log(action, selection);
-      this.eachQuestion = [action, selection];
+      this.eachQuestion.push = ([action, selection]);
+      console.log(this.eachQuestion);
+      const filtered = this.actions.filter((x) => x !== action);
+      if (selection === 'yes') {
+        filtered.map((x) => document.getElementById(x).classList.add('is-hidden'));
+        this.consSection = true;
+      } else {
+        filtered.map((x) => document.getElementById(x).classList.remove('is-hidden'));
+      }
     },
   },
 //   mounted() {
